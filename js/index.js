83b58b3c0dd590f743588b66a5509f0b927d12b3
Noel Jane Bourg <noeljanebourg@gmail.com>
2020-01-20 12:06:08 -0700

    add fetch

--- js/index.js
+++ js/index.js
@@ -1,11 +1,35 @@
-JSC.Chart('chartDiv', {
-  type: 'horizontal column',
-  series: [
-    {
-      points: [
-        {x: 'Apples', y:50},
-        {x:'Oranges', y:42}
-      ]
-    }
-  ]
-});
+// JSC.Chart('chartDiv', {
+//   type: 'horizontal column',
+//   series: [
+//     {
+//       name: 'Andy',
+//       points: [
+//         {x: 'Apples', y:50},
+//         {x:'Oranges', y:42}
+//       ]
+//     },
+//     {
+//       name: 'Anna', 
+//       points: [
+//         {x: 'Apples', y: 30}, 
+//         {x: 'Oranges', y: 22}
+//       ]
+//     }
+//   ]
+// });
+
+fetch('https://data.cdc.gov/resource/w9j2-ggv5.csv')
+  .then(function (response) {
+    return response.text();
+  })
+  .then(function (text) {
+    csvToSeries(text);
+  })
+  .catch(function (error) {
+    //Something went wrong
+    console.log(error);
+  })
+  
+function csvToSeries(text) {
+  console.log(text);
+}
