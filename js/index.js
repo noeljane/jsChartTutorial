745d1bdf157e2a3edaf261d607297b3b6615d95e
Noel Jane Bourg <noeljanebourg@gmail.com>
2020-01-20 12:49:21 -0700

    display chart

--- js/index.js
+++ js/index.js
@@ -1,42 +1,40 @@
 // https://www.freecodecamp.org/news/how-to-make-your-first-javascript-chart/
 
 fetch('https://data.cdc.gov/resource/w9j2-ggv5.csv')
-  .then(function (response) {
-    return response.text();
-  })
-  .then(function (text) {
-    let series = csvToSeries(text);
+	.then(function (response) {
+		return response.text();
+	})
+	.then(function (text) {
+		let series = csvToSeries(text);
 		renderChart(series);
-  })
-  .catch(function (error) {
-    //Something went wrong
-    console.log(error);
-  })
+	})
+	.catch(function (error) {
+		//Something went wrong
+		console.log(error);
+	});
 
 function csvToSeries(text) {
-  const lifeExp = 'average_life_expectancy';
-  let dataAsJson = JSC.csv2Json(text);
-  let male = [], female = [];
-  dataAsJson.forEach(function (row) {
-    //add either to male, female, or discard.
-    if (row.race === 'All Races') {
-      if (row.sex === 'Male') {
-        male.push({x: row.year, y: row[lifeExp]});
-      } else if (row.sex === 'Female') {
-        female.push({x: row.year, y: row[lifeExp]})
-      }
-    }
-    let data =  [
-      {name: 'Male', points: male},
-      {name: 'Female', points: female}
-    ];
-    console.log(data);
-    return data;
-  })
+	const lifeExp = 'average_life_expectancy';
+	let dataAsJson = JSC.csv2Json(text);
+	let male = [], female = [];
+	dataAsJson.forEach(function (row) {
+		//add either to male, female, or discard.
+		if (row.race === 'All Races') {
+			if (row.sex === 'Male') {
+				male.push({x: row.year, y: row[lifeExp]});
+			} else if (row.sex === 'Female') {
+				female.push({x: row.year, y: row[lifeExp]});
+			}
+		}
+	});
+	return [
+		{name: 'Male', points: male},
+		{name: 'Female', points: female}
+	];
 }
 
 function renderChart(series) {
-  JSC.Chart('chartDiv', {
-    series: series
-  })
+	JSC.Chart('chartDiv', {
+		series: series
+	});
 }
